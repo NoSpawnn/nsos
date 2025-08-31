ed37ea157c7f3e042aef9733f0f57ac5544d0f07
NoSpawnn <adam.blighe@protonmail.com>
2025-08-31 21:03:04 +0100

    Add flatpak systemd services

--- files/scripts/ublue.sh
+++ files/scripts/ublue.sh
@@ -11,11 +11,6 @@ unzip -q $TEMP/packages.zip -d $TEMP/
 echo "Copying ublue udev rules"
 cp $TEMP/packages-main/packages/ublue-os-udev-rules/src/udev-rules.d/*.rules /usr/lib/udev/rules.d/
 
-echo "Copying ublue flatpak updater"
-cp $TEMP/packages-main/packages/ublue-os-update-services/src/usr/lib/systemd/system/flatpak-system-update.timer /usr/lib/systemd/system/flatpak-system-update.timer
-cp $TEMP/packages-main/packages/ublue-os-update-services/src/usr/lib/systemd/system/flatpak-system-update.service /usr/lib/systemd/system/flatpak-system-update.service
-cp $TEMP/packages-main/packages/ublue-os-update-services/src/usr/lib/systemd/user/flatpak-user-update.timer /usr/lib/systemd/user/flatpak-user-update.timer
-cp $TEMP/packages-main/packages/ublue-os-update-services/src/usr/lib/systemd/user/flatpak-user-update.service /usr/lib/systemd/user/flatpak-user-update.service
 echo "Enabling systemd timers for flatpak updaters"
 systemctl --system enable flatpak-system-update.timer
 systemctl --global enable flatpak-user-update.timer
